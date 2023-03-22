c9d55586da841fc458c5bcd769adf91f924569a4
Ehsan <haqehs12@gmail.com>
2023-03-22 11:36:57 +0530

    ur

--- addUp.js
+++
@@ -1,9 +0,0 @@
-function addUp(number){
-	let total = 0;
-
-	for(i = 0;i <= number; i++){
-		total+=i;
-	} 
-		return total;
-}
-console.log(addUp(5))
--- returnElement.js
+++
@@ -1,4 +0,0 @@
-function getFirstValue(array){
-	return array[0];
-}
-console.log(getFirstValue([1,13,14,15]));
