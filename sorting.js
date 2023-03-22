e68fc24272e6a98799c29fc1e62337b74d125b3d
Ehsan <haqehs12@gmail.com>
2023-03-22 13:42:02 +0530

    Bug Found

--- sorting.js
+++ sorting.js
@@ -12,19 +12,3 @@ for(i = 0;i < len; i++)
 }
 }
 console.log(data)
-
-// let data = [34,3,1,25,54,12,89,5,76,789];
-
-
-//       for(i=0;i<data.length;i++){
-
-//         for(j=0;j<data.length;j++){
-
-//             if(data[j]>data[j+1]){
-//                 let temp=data[j];
-//                 data[j]=data[j+1];
-//                 data[j+1]=temp;
-//             }
-//         }
-//       }
-//       console.log(data)
