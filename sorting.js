70f3e3778c8d249aa0be1ac57f1bb83cfe8865f5
Ehsan <haqehs12@gmail.com>
2023-03-22 13:23:55 +0530

    Bug Found

---
+++ sorting.js
@@ -0,0 +1,14 @@
+let data = [2,1,3,6,5,8,0];
+let len = data.length;
+for(i = 0;i < len; i++)
+{
+	for(j = 0;j < len; j++)
+	{
+		if(data[j] > data[j+1])
+
+			let temp = data[j];
+			data[j] = data[j+1];
+			temp = data[j+1];
+	}
+}
+console.log(data)
